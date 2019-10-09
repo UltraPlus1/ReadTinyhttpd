b124503506f9c7b315808790cc8b3d141cce0539
MasonZhang <1076536974@qq.com>
2019-10-09 17:41:26 +0800

    添加了tinyhttpd 的源码，并写好了readme

---
+++ simpleclient.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    char ch = 'A';
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = htons(34819);
+    len = sizeof(address);
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+
+    if (result == -1)
+    {
+        perror("oops: client1");
+        _exit(1);
+    }
+    write(sockfd, buf, sizeof(buf)); /*发送请求*/
+    read(sockfd, get, sizeof(get));  /*接收返回数据*/
+    /*打印返回数据*/
+    printf("char from server = %c\n", ch);
+    close(sockfd);
+    _exit(0);
+}
